d391df1efb580dc76f5d8999ab41dd96e05069d5
RaniAdkine <88865525+RaniAdkine@users.noreply.github.com>
2026-03-08 14:22:00 +0530

    Add Maven installation to jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,7 @@
 sudo apt update
 sudo apt install fontconfig openjdk-21-jre
 java -version
+sudo apt install maven -y
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
