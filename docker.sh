6e2ebe725909767020558eec77feaabd19011c79
RaniAdkine <88865525+RaniAdkine@users.noreply.github.com>
2026-03-08 14:15:09 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+#To install the latest version, run
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl status docker
+
+#Verify that the installation is successful by running the hello-world image:
+# sudo docker run hello-world
